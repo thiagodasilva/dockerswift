96c8d8a47fd73bffa58f8035f8b6c541f512db7e
Thiago da Silva <thiago@redhat.com>
2017-03-08 13:24:34 -0500

    Added vagrant and ansible roles, plus new deployment model
    The vagrant + ansible roles builds a 4 node k8s cluster. A master
    node and 3 storage nodes.
    Also added a new deployment model for kubernetes, see swift_k8s

--- config/swift/rings/make_rings.sh
+++ config/swift/rings/make_rings.sh
@@ -1,12 +1,26 @@
-swift-ring-builder account.builder create 8 1 0
-swift-ring-builder container.builder create 8 1 0
-swift-ring-builder object.builder create 8 1 0
-
-swift-ring-builder account.builder add r1z1-127.0.0.1:6202/sda_ 1
-swift-ring-builder container.builder add r1z1-127.0.0.1:6201/sda_ 1
-swift-ring-builder object.builder add r1z1-127.0.0.1:6200/sda_ 1
-
+swift-ring-builder account.builder create 8 3 0
+swift-ring-builder account.builder add r1z1-10.44.0.2:6202/sdb1_ 1
+swift-ring-builder account.builder add r1z1-10.44.0.2:6202/sdc1_ 1
+swift-ring-builder account.builder add r1z1-10.44.0.2:6202/sdd1_ 1
+swift-ring-builder account.builder add r1z1-10.47.0.1:6202/sdb1_ 1
+swift-ring-builder account.builder add r1z1-10.47.0.1:6202/sdc1_ 1
+swift-ring-builder account.builder add r1z1-10.47.0.1:6202/sdd1_ 1
 swift-ring-builder account.builder rebalance
-swift-ring-builder container.builder rebalance
-swift-ring-builder object.builder rebalance
 
+swift-ring-builder container.builder create 8 2 0
+swift-ring-builder container.builder add r1z1-10.47.0.1:6201/sdb1_ 1
+swift-ring-builder container.builder add r1z1-10.47.0.1:6201/sdc1_ 1
+swift-ring-builder container.builder add r1z1-10.47.0.1:6201/sdd1_ 1
+swift-ring-builder container.builder add r1z1-10.44.0.2:6201/sdb1_ 1
+swift-ring-builder container.builder add r1z1-10.44.0.2:6201/sdc1_ 1
+swift-ring-builder container.builder add r1z1-10.44.0.2:6201/sdd1_ 1
+swift-ring-builder container.builder rebalance
+
+swift-ring-builder object.builder create 8 2 0
+swift-ring-builder object.builder add r1z1-10.47.0.1:6200/sdb1_ 1
+swift-ring-builder object.builder add r1z1-10.47.0.1:6200/sdc1_ 1
+swift-ring-builder object.builder add r1z1-10.47.0.1:6200/sdd1_ 1
+swift-ring-builder object.builder add r1z1-10.44.0.2:6200/sdb1_ 1
+swift-ring-builder object.builder add r1z1-10.44.0.2:6200/sdc1_ 1
+swift-ring-builder object.builder add r1z1-10.44.0.2:6200/sdd1_ 1
+swift-ring-builder object.builder rebalance
